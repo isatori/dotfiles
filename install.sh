dd28fbd8aae387c99aa5a70f40730a68a2e620c5
isatori <isshin.satori@gmail.com>
2018-12-19 16:39:22 -0800

    install

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt upgrade
 
 # install essential programs
-sudo apt install build-essential curl wget nmap awesome awesome-extra xfonts-terminus 
+sudo apt install build-essential curl wget nmap awesome awesome-extra xfonts-terminus -y
 
 # install enhancements
-sudo apt install volti
+sudo apt install volti unclutter scrot mpd mpc dmenu xsel i3lock -y
